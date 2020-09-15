7cfe02794ffccbc31dc2df88a98a8df187e3a397
masa <15242577+masaka27@users.noreply.github.com>
2020-09-16 00:48:33 +0900

    second push

--- js/index.js
+++ js/index.js
@@ -1,9 +1,49 @@
-$(function () {
-  var biggestHeight = '0';
-  $('.parent').each(function () {
-    if ($(this).height() > biggestHeight) {
-      biggestHeight = $(this).height();
-    }
+//nav-menu-animation
+const navSlide = () => {
+  const burger = document.querySelector('.nav-btn');
+  const nav = document.querySelector('.header');
+  const navLink = document.querySelectorAll('.nav-outer li');
+
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+
+    //Animation links
+
+    navLink.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = '';
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 8 + 0.2
+        }s`;
+        console.log(index / 8 + 0.2);
+      }
+    });
+    //burger Toggle
+    burger.classList.toggle('burger-open');
   });
-  $('.parent').height(biggestHeight);
+};
+navSlide();
+
+//slide-animation
+const reverl = gsap.utils.toArray('.cover-slide-txt');
+reverl.forEach((txt, i) => {
+  ScrollTrigger.create({
+    trigger: txt,
+    toggleClass: 'active',
+    start: 'top 80%',
+    end: 'top 10%',
+    markers: true,
+  });
+});
+//img-animation
+const images = gsap.utils.toArray('.cover-slide-img');
+images.forEach((img, i) => {
+  ScrollTrigger.create({
+    trigger: img,
+    toggleClass: 'active',
+    start: 'top 90%',
+    end: 'top 10%',
+    markers: true
+  })
 });
