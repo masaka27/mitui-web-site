9d88ed711ef0a21537bd5bb6a10e695a1e7371b9
masamichi <15242577+masaka27@users.noreply.github.com>
2020-09-16 15:11:34 +0900

    third push

--- js/index.js
+++ js/index.js
@@ -2,13 +2,12 @@
 const navSlide = () => {
   const burger = document.querySelector('.nav-btn');
   const nav = document.querySelector('.header');
-  const navLink = document.querySelectorAll('.nav-outer li');
+  const navLink = document.querySelectorAll('.nav-inner li');
 
   burger.addEventListener('click', () => {
     nav.classList.toggle('nav-active');
 
     //Animation links
-
     navLink.forEach((link, index) => {
       if (link.style.animation) {
         link.style.animation = '';
@@ -16,7 +15,6 @@ const navSlide = () => {
         link.style.animation = `navLinkFade 0.5s ease forwards ${
           index / 8 + 0.2
         }s`;
-        console.log(index / 8 + 0.2);
       }
     });
     //burger Toggle
@@ -25,6 +23,18 @@ const navSlide = () => {
 };
 navSlide();
 
+//fade-animation
+const fade = gsap.utils.toArray('.fade');
+fade.forEach((act, i) => {
+  ScrollTrigger.create({
+    trigger: act,
+    toggleClass: 'active',
+    start: 'top 80%',
+    // end: 'bottom top',
+    markers: false,
+  });
+});
+
 //slide-animation
 const reverl = gsap.utils.toArray('.cover-slide-txt');
 reverl.forEach((txt, i) => {
@@ -32,8 +42,8 @@ reverl.forEach((txt, i) => {
     trigger: txt,
     toggleClass: 'active',
     start: 'top 80%',
-    end: 'top 10%',
-    markers: true,
+    end: 'bottom top',
+    markers: false,
   });
 });
 //img-animation
@@ -42,8 +52,38 @@ images.forEach((img, i) => {
   ScrollTrigger.create({
     trigger: img,
     toggleClass: 'active',
-    start: 'top 90%',
-    end: 'top 10%',
-    markers: true
-  })
+    start: 'top 80%',
+    end: 'bottom top',
+    markers: false,
+  });
+});
+// firstview opening animation
+const timeline = gsap.timeline({ defaults: { duration: 1 } });
+timeline
+  .from('.header-side', { x: '100%', ease: 'power2.in' })
+  .from('.hero-container', { x: '-100%', ease: 'elastic.out(1, 0.3)' })
+  .from('.section1', { y: '100px', ease: 'bounce' });
+
+// sprit text animation
+Splitting();
+const targetTxt = document.querySelectorAll('.text-animation');
+const xValues = ['0%', '70%', '300%', '0%', '70%', '300%'];
+const yValues = ['0%', '70%', '-100%', '0%', '70%', '-100%'];
+targetTxt.forEach((current, index, array) => {
+  gsap.from(current.querySelectorAll('.char'), {
+    scrollTrigger: {
+      trigger: current,
+      start: 'top 70%',
+      end: 'bottom top',
+      pin: false,
+      scrub: false,
+    },
+    opacity: 0,
+    scale: 0.1,
+    x: xValues[index],
+    y: yValues[index],
+    stagger: {
+      amount: 1.5,
+    },
+  });
 });
